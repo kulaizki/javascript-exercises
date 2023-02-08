4859663d862ff77a0e2f4924144a7efd79cac219
kulaizki <kulaizke@gmail.com>
2023-02-08 20:20:34 +0800

    repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    if (count < 0) return "ERROR";
+    return string.repeat(count);
 };
 
 // Do not edit below this line

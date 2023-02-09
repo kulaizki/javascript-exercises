a4c19234ff3bd1bc10297e47a063c4ea3636b192
kulaizki <kulaizke@gmail.com>
2023-02-09 20:37:53 +0800

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || isNaN(start) || isNaN(end) || 
+        typeof(start) != "number" || typeof(end) != "number")
+            return "ERROR";
 
+    let sum = 0;
+    let i = start;
+    let n = end;
+
+    if (start > end) {
+        i = end;
+        n = start;
+    }
+
+    for (;i <= n; i++) {
+        sum += i
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

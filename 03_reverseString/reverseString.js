18fc993db944b8e76e3169460b107c3d03a6648e
kulaizki <kulaizke@gmail.com>
2023-02-09 19:45:52 +0800

    done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split(""); // splits the characters, split(" ") splits words
+    const reverseArray = array.reverse();
+    return reverseArray.join(""); // concatenates all characters in the array into one string
 };
 
 // Do not edit below this line

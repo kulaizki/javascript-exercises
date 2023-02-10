997ced8c036e5b00d3a1f13a805064f91e29d912
kulaizki <kulaizke@gmail.com>
2023-02-10 10:03:45 +0800

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return Math.round((temp - 32) * 5/9 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Math.round((temp * 9/5 + 32) * 10) / 10;
 };
+
+  // Alternative
+  // let convert = temp * 9/5 + 32;
+  // let result = convert.toFixed(1);
+  // return parseFloat(result);
 
 // Do not edit below this line
 module.exports = {

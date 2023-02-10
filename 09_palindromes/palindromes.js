f9c0818361dfc48fb5b04c1a1a34afc9ccce0ab4
kulaizki <kulaizke@gmail.com>
2023-02-10 10:43:47 +0800

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let lowercaseStr = str.toLowerCase().replace(/[^a-z]/g, "");
+  return lowercaseStr === lowercaseStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// Alternative
+// let lowercaseStr = str.toLowerCase();
+// let charactersArr = lowercaseStr.split("");
+// let validCharacters = "abcdefghijklmnopqrstuvwxyz".split("");
+
+// let lettersArr = [];
+// charactersArr.forEach((char) => {
+//   if (validCharacters.indexOf(char) > -1) lettersArr.push(char);
+// });
+
+// return lettersArr.join("") === lettersArr.reverse().join("");

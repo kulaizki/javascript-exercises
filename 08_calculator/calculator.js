47f286f0edda61e3aad444eb86e662f01e3aee90
kulaizki <kulaizke@gmail.com>
2023-02-10 10:25:15 +0800

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let arraySum = 0;
+  for (let i = 0 ; i < arr.length; ++i) {
+    arraySum += arr[i];
+  }
+  return arraySum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	let arrayProduct = 1;
+  for (let i = 0 ; i < arr.length; ++i) {
+    arrayProduct *= arr[i];
+  }
+  return arrayProduct;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  return Math.pow(base, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	fact = 1;
+  for (let i = 2; i <= n; ++i) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line

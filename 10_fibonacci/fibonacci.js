c5c920eb78efede0791d89f951453e305ec68b1c
kulaizki <kulaizke@gmail.com>
2023-02-10 11:04:47 +0800

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) 
+        return "OOPS";
+    if (typeof(n) === "string")
+        n = Number(n);
 
+    let n1 = 0, n2 = 1, sum = 1;
+    for (let i = 1; i < n; ++i) {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
